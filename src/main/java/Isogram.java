50ed40b57337872ed2ef581bf9e658767120298d
phorjax <joseph.12082@gmail.com>
2023-07-05 20:45:56 +0000

    done

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -13,6 +13,13 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
